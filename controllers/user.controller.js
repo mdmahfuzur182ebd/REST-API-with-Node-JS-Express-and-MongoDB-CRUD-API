e28e1558cba170183f4f946db1546e37845abf49
Mahfuzur Rahman <mdmahfuzur980@gmail.com>
2022-07-27 00:13:17 +0600

    some Problem

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,7 @@
+const User = require("../models/user.model");
+const { v4: uuidv4 } = require ("uuid");
+
+
 const getAllUsers = (req, res) => {
     res.status(200).json({
         massage: "all users",
@@ -10,10 +14,19 @@ const getOneUsers = (req, res) => {
     });
 };
 
-const createUser = (req, res) => {
-    res.status(201).json({
-        massage: "Create user",
+const createUser = async(req, res) => {
+   try{
+       const newUser = new User({
+         id: uuidv4(),
+         name: req.body.name,
+         age: Number(req.body.age),
     });
+    await newUser.save();
+    res.status(201).json(newUser);
+   }
+   catch ( error) {
+    res.status(500).send (error.massage)
+   }
 };
 
 const updateUser = (req, res) => {
